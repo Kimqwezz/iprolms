429c5a467ea1af5bf4911b3a9f0c0173e4a96bf6
kimqwezz <kim569653@mail.ru>
2025-04-16 10:54:33 +0600

    rename layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <x-app-layout>
+    <x-app>
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Dashboard') }}
@@ -22,6 +22,6 @@
                 </div>
             </div>
         </div>
-    </x-app-layout>
+    </x-app>
 </body>
 </html>
